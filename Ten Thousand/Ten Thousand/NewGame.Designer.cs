cde55312c4d5ac48920410767036e9d8d7172e2a
Parshva Shah <Parshva Shah>
2016-08-11 18:19:31 -0400

    Added New Game form

---
+++ Ten Thousand/Ten Thousand/NewGame.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Ten_Thousand
+{
+    partial class NewGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.nameList = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // playBtn
+            // 
+            this.playBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+            this.playBtn.Location = new System.Drawing.Point(259, 301);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(96, 36);
+            this.playBtn.TabIndex = 11;
+            this.playBtn.Text = "Play 10K";
+            this.playBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+            this.addBtn.Location = new System.Drawing.Point(140, 301);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(77, 36);
+            this.addBtn.TabIndex = 10;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(24, 301);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(74, 36);
+            this.clearBtn.TabIndex = 9;
+            this.clearBtn.Text = "Clear Names";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            // 
+            // nameList
+            // 
+            this.nameList.Location = new System.Drawing.Point(81, 101);
+            this.nameList.Multiline = true;
+            this.nameList.Name = "nameList";
+            this.nameList.ReadOnly = true;
+            this.nameList.Size = new System.Drawing.Size(202, 161);
+            this.nameList.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 34);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter names of the players:";
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTxt.Location = new System.Drawing.Point(81, 52);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(202, 42);
+            this.nameTxt.TabIndex = 6;
+            // 
+            // NewGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 353);
+            this.Controls.Add(this.playBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.nameList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTxt);
+            this.Name = "NewGame";
+            this.Text = "NewGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.TextBox nameList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTxt;
+    }
+}
